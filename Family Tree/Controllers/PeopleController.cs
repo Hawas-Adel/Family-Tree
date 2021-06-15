9cd7f6770659a774c9a42833751d147d2193311b
Hawas-Adel <Hawas-Adel@users.noreply.github.com>
2021-06-15 18:32:04 +0200

    Initial commit

---
+++ Family Tree/Controllers/PeopleController.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Family_Tree;
+
+namespace Family_Tree.Controllers
+{
+	public class PeopleController : Controller
+	{
+		private Family_DBEntities db = new Family_DBEntities();
+
+		// GET: People
+		public ActionResult Index()
+		{
+			var people = db.People.Include(p => p.Person1).Include(p => p.Person2);
+			return View(people.ToList());
+		}
+
+		// GET: People/Details/5
+		public ActionResult Details(int? id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			Person person = db.People.Find(id);
+			if (person == null)
+			{
+				return HttpNotFound();
+			}
+			List<(Person Relative, string Relation)> Relatives = new List<(Person Relative, string Relation)>();
+			if (person.FatherID.HasValue)
+			{
+				var Father = db.People.Find(person.FatherID.Value);
+				Relatives.Add((Father, "Father"));
+			}
+			if (person.MotherID.HasValue)
+			{
+				var Mother = db.People.Find(person.MotherID.Value);
+				Relatives.Add((Mother, "Mother"));
+			}
+			if (person.FatherID.HasValue)
+			{
+				var Siblings = db.People.Where(P => P.FatherID == person.FatherID && P.ID != person.ID);
+				foreach (var item in Siblings)
+					Relatives.Add((item, "Sibling"));
+			}
+			if (person.MotherID.HasValue)
+			{
+				var Siblings = db.People.Where(P => P.MotherID == person.MotherID && P.ID != person.ID);
+				foreach (var item in Siblings)
+					Relatives.Add((item, "Sibling"));
+			}
+			var Children = db.People.Where(P => (person.Gender && P.MotherID == person.ID) || (!person.Gender && P.FatherID == person.ID));
+			foreach (var item in Children)
+				Relatives.Add((item, "Child"));
+
+			Relatives = Relatives.Distinct().ToList();
+
+			return View((person, Relatives));
+		}
+
+		// GET: People/Create
+		public ActionResult Create()
+		{
+			InitializeSelectLists();
+			return View();
+		}
+
+		//private void InitializeSelectLists()
+		//{
+		//	ViewBag.FatherID = new SelectList(db.People.Where(P => !P.Gender), "ID", "FirstName");
+		//	ViewBag.MotherID = new SelectList(db.People.Where(P => P.Gender), "ID", "FirstName");
+		//}
+		private void InitializeSelectLists(Person person =null)
+		{
+			ViewBag.FatherID = new SelectList(db.People.Where(P => !P.Gender).ToList().ConvertAll(P => new { ID = P.ID, FullName = $"{P.FirstName} {P.LastName}" }),
+				"ID", "FullName", person?.FatherID);
+			ViewBag.MotherID = new SelectList(db.People.Where(P => P.Gender).ToList().ConvertAll(P => new { ID = P.ID, FullName = $"{P.FirstName} {P.LastName}" }),
+				"ID", "FullName", person?.MotherID);
+		}
+
+		// POST: People/Create
+		// To protect from overposting attacks, enable the specific properties you want to bind to, for 
+		// more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Create([Bind(Include = "ID,FirstName,LastName,Gender,FatherID,MotherID")] Person person)
+		{
+			if (ModelState.IsValid)
+			{
+				db.People.Add(person);
+				db.SaveChanges();
+				return RedirectToAction("Index");
+			}
+
+			InitializeSelectLists(person);
+			return View(person);
+		}
+
+		// GET: People/Edit/5
+		public ActionResult Edit(int? id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			Person person = db.People.Find(id);
+			if (person == null)
+			{
+				return HttpNotFound();
+			}
+			InitializeSelectLists(person);
+			return View(person);
+		}
+
+		// POST: People/Edit/5
+		// To protect from overposting attacks, enable the specific properties you want to bind to, for 
+		// more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Edit([Bind(Include = "ID,FirstName,LastName,Gender,FatherID,MotherID")] Person person)
+		{
+			if (ModelState.IsValid)
+			{
+				db.Entry(person).State = EntityState.Modified;
+				db.SaveChanges();
+				return RedirectToAction("Index");
+			}
+			InitializeSelectLists(person);
+			return View(person);
+		}
+
+		// GET: People/Delete/5
+		public ActionResult Delete(int? id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			Person person = db.People.Find(id);
+			if (person == null)
+			{
+				return HttpNotFound();
+			}
+			return View(person);
+		}
+
+		// POST: People/Delete/5
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		public ActionResult DeleteConfirmed(int id)
+		{
+			try
+			{
+				Person person = db.People.Find(id);
+				db.People.Remove(person);
+				db.SaveChanges();
+			}
+			catch (Exception)
+			{
+
+			}
+			return RedirectToAction("Index");
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				db.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+	}
+}
---
+++ Family Tree/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Family_Tree")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("by adguard")]
+[assembly: AssemblyProduct("Family_Tree")]
+[assembly: AssemblyCopyright("Copyright © by adguard 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("dfbeb675-bd9b-4567-82ec-5b73aa641e9e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
